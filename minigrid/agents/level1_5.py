b0407f1841e199d8f28a09eb792e900396789024
Axel Grard <phidoraxgame@gmail.com>
2023-05-03 17:27:55 +0200

    Ajout des arguments pour le nombre d'épisodes pour le train et le test

--- minigrid/agents/level1_5.py
+++ minigrid/agents/level1_5.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+from argparse import ArgumentParser
+
 from minigrid.utils.window import Window
 from minigrid.wrappers import QLearningWrapper, AgentObsWrapper, ObjectifWrapper
 import gymnasium as gym
@@ -7,7 +9,13 @@ import matplotlib
 matplotlib.use('TkAgg')
 
 # Create the environment
-env = gym.make('MiniGrid-FourRooms-v0')
+parser = ArgumentParser()
+parser.add_argument("--env", help="gym environment to load", default="MiniGrid-FourRooms-v0")
+parser.add_argument("--train", help="number of episode during training", default=50)
+parser.add_argument("--test", help="number of episode during testing", default=10)
+args = parser.parse_args()
+# Create the environment
+env = gym.make(args.env)
 env = ObjectifWrapper(env)
 env = AgentObsWrapper(env)
 
@@ -17,9 +25,9 @@ ql_wrapper = QLearningWrapper(env)
 # Train the agent
 
 window = Window("Level 1.5")
-ql_wrapper.train(num_episodes=50)
+ql_wrapper.train(num_episodes=args.train)
 
 # Test the agent
-ql_wrapper.test(num_episodes=10,window=window)
+ql_wrapper.test(num_episodes=args.test, window=window)
 
 window.show(True)
